a7b7b5ca71989c91f4e5f39a3e7c4f511f1e80eb
Adolfo Zepeda <adolfitozepeda@gmail.com>
2025-05-17 18:55:08 -0700

    resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+const LIVE_URL = "https://github.com/adolfin07/TiimTec"
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://github.com/adolfin07',
     vite: {
         plugins: [tailwindcss()],
     },
-    site:'https://github.com/adolfin07',
-    base:'TiimTec',
 });
